cc9a53e953f596074524b518642336cc0cd12d0b
Your Name <you@example.com>
2024-10-20 20:30:31 +0800

    Updated settings and requirements for deployment

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 REST_FRAMEWORK = {
