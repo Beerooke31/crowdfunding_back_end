7dc559e5c1977e7621599cff8c3bd9f7e6fbeae4
Your Name <you@example.com>
2024-10-26 21:21:58 +0800

    Added screenshots, project and user registration steps.

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -1,12 +1,13 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, permissions
+from rest_framework import status, permissions     
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token 
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 from .permissions import IsUserOrReadOnly
+
 
 class CustomUserList(APIView):
     permission_classes = [permissions.IsAuthenticated, IsUserOrReadOnly]
