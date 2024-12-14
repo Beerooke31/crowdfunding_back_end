8fb5f19a9ee93284b8407a2a4415ac62b7560bb8
Your Name <you@example.com>
2024-12-14 10:08:32 +0800

    Changed return on permissions to true

--- crowdfunding/users/permissions.py
+++ crowdfunding/users/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 
 class IsUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        # if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.username == request.user.username
+        # return obj.username == request.user.username
 
