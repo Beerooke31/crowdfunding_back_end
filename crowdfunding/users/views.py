64219780782e3fcae37908c0cc7fa85ace601b22
Your Name <you@example.com>
2025-01-12 17:10:38 +0800

    Removed commented out code and updated Readme

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -10,7 +10,6 @@ from .permissions import IsUserOrReadOnly
 
 
 class CustomUserList(APIView):
-    # permission_classes = [permissions.IsAuthenticated, IsUserOrReadOnly]
 
     def get(self, request):
         users = CustomUser.objects.all()
